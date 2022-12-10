d8aa49561e326b6a68ee4a25d67bb9772ab7368c
Thomas Maxwell <maxxabillion@asatera.com>
2022-12-10 13:34:28 -0500

    updated article model schema

--- models/article.model.js
+++ models/article.model.js
@@ -1,3 +1,4 @@
+const { Double } = require('bson');
 const mongoose = require('mongoose')
 
 var articlesSchema = new mongoose.Schema({
@@ -6,6 +7,15 @@ var articlesSchema = new mongoose.Schema({
     description: String,
     publishedAt: Date,
     source_id: String,
+    GPE: String,
+    ORG: String,
+    PERSON: String,
+    sentiment: String,
+    sentiment_score: Double,
+    sentiment_time: Double,
+    Tokens: Array,
+    Topic: String,
+    Topic_Contribution: Double,
     summarization: String,
     title: String,
     url: String,
