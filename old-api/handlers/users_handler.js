ffc84eb38df2d9964e4801751e121b8a972f352f
ketan <somaniketan74@gmail.com>
2023-04-09 23:37:05 +0530

    changes

--- old-api/handlers/users_handler.js
+++ old-api/handlers/users_handler.js
@@ -21,7 +21,7 @@ import * as querystring from "querystring";
 import AWS from "aws-sdk";
 import * as AmazonCognitoIdentity from "amazon-cognito-identity-js";
 import {subWeeks} from "date-fns";
-import { ObjectId } from 'mongoose';
+import mongoose from 'mongoose';
 
 const poolData = {
   UserPoolId: process.env.COGNITO_USER_POOL,
@@ -744,7 +744,7 @@ export async function post_confirmation (event, context, callback) {
                     email: event.request.userAttributes['email'],
                     apiKey: randomKey.replace(/-/g, ""),
                     cognito_id: event.request.userAttributes['sub'],
-                    plan: ObjectId('63f11ba3b1ab44c5ae9f3bfd')
+                    plan: mongoose.Types.ObjectId('63f11ba3b1ab44c5ae9f3bfd')
                 });
                 let user = await newUser.save();
                 console.log(user);
